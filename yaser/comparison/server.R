5d2dc0c34f2c761ada70b263c6141336443fda17
Stephen Davies <stephen@umw.edu>
2015-05-28 12:59:12 -0400

    (Rename "data".)

--- yaser/comparison/server.R
+++ yaser/comparison/server.R
@@ -11,13 +11,13 @@ shinyServer(function(input,output,session) {
         if (input$runit < 1) return(NULL)
 
         isolate({
-            data <<- generate.data(input$npts, covar.1=input$cov1,
+            the.data.set <<- generate.data(input$npts, covar.1=input$cov1,
                 covar.neg1=input$covn1, 
                 mean.x1.neg1=input$classn1meanx1,
                 mean.x2.neg1=input$classn1meanx2,
                 prior=input$prior,
                 gaussianness=input$gaussianness)
-            p <- ggplot(data$training) + 
+            p <- ggplot(the.data.set$training) + 
                 geom_point(aes(x=x1, y=x2, color=y)) +
                 xlim(-3,6) +
                 ylim(-3,6) +
@@ -30,8 +30,8 @@ shinyServer(function(input,output,session) {
     output$performancePlot <- renderPlot({
         if (input$runit < 1) return(NULL)
         plot.data <- data.frame(alg=factor(c("LDA","QDA","Log reg")),
-            perf=c(lda.accuracy(data),qda.accuracy(data),
-                                                log.reg.accuracy(data))
+            perf=c(lda.accuracy(the.data.set),qda.accuracy(the.data.set),
+                                                log.reg.accuracy(the.data.set))
         )
         p <- ggplot(plot.data) + 
             geom_bar(aes(x=alg,fill=alg,y=perf),stat="identity") +
