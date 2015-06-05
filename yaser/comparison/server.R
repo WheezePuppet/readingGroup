6c0f9ab2733c818c069e96eaf63d44bf110456aa
Stephen Davies <stephen@umw.edu>
2015-06-05 10:27:29 -0400

    Refactor Shiny reactivity.

--- yaser/comparison/server.R
+++ yaser/comparison/server.R
@@ -6,21 +6,28 @@ shinyServer(function(input,output,session) {
 
     source("comparison.R")
 
+    values <- reactiveValues(
+        the.data.set=NULL
+    )
+
+    observeEvent(input$runit, {
+        values$the.data.set <- generate.data(input$npts, 
+            covar.neg1=matrix(c(
+                input$classn1var1,
+                input$classn1covar12,
+                input$classn1covar12,
+                input$classn1var2), nrow=2),
+            mean.neg1=c(input$classn1meanx1,input$classn1meanx2),
+            prior=input$prior,
+            gaussianness=input$gaussianness)
+    })
+
     output$mainPlot <- renderPlot({
 
         if (input$runit < 1) return(NULL)
 
         isolate({
-            the.data.set <<- generate.data(input$npts, 
-                covar.neg1=matrix(c(
-                    input$classn1var1,
-                    input$classn1covar12,
-                    input$classn1covar12,
-                    input$classn1var2), nrow=2),
-                mean.neg1=c(input$classn1meanx1,input$classn1meanx2),
-                prior=input$prior,
-                gaussianness=input$gaussianness)
-            p <- ggplot(the.data.set$training) + 
+            p <- ggplot(values$the.data.set$training) + 
                 geom_point(aes(x=x1, y=x2, color=y)) +
                 xlim(-3,6) +
                 ylim(-3,6) +
@@ -34,9 +41,9 @@ shinyServer(function(input,output,session) {
         if (input$runit < 1) return(NULL)
         plot.data <- data.frame(alg=factor(c("LDA","QDA","Log reg"),
                 levels=c("LDA","QDA","Log reg")),
-            perf=sapply(list(run.lda(the.data.set),
-                          run.qda(the.data.set),
-                          run.log.reg(the.data.set)),
+            perf=sapply(list(run.lda(values$the.data.set),
+                          run.qda(values$the.data.set),
+                          run.log.reg(values$the.data.set)),
                 function(thing) thing$accuracy)
         )
         p <- ggplot(plot.data) + 
@@ -45,4 +52,11 @@ shinyServer(function(input,output,session) {
             ggtitle("Classifier performance")
         print(p)
     })
+
+    output$posteriorPlot <- renderPlot({
+
+
+    })
+
+
 })
