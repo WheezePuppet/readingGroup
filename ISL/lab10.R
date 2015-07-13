6e0e16eb5bcb91396d9815b0b8503ab8fd2783c8
Stephen Davies <stephen@umw.edu>
2015-07-13 12:11:25 -0400

    Lab 10.5.1 for grades data.

---
+++ ISL/lab10.R
@@ -0,0 +1,18 @@
+
+unsup.grades <- grades %>% select(quizzes:gender)
+unsup.grades$gender <- as.numeric(unsup.grades$gender)
+row.names(unsup.grades) <- grades$Name
+# (Now need to fix to ensure no dups.)
+
+sc.unsup.grades <- scale(unsup.grades)
+
+# cluster based on (scaled) original features
+km.out <- kmeans(sc.unsup.grades,3,nstart=50)
+
+# Plot first two principal components and cluster
+g <- ggplot(data.frame(name=row.names(sc.unsup.grades),
+                       x=prcomp(sc.unsup.grades)$x[,1:2],
+                       y=factor(km.out$cluster))) +
+    geom_text(aes(x=x.PC1,y=x.PC2,col=y,label=name),size=2.5)
+print(g)
+
