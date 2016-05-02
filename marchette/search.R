f04e9047001c31d01b196912ed60c9f69eacdb9d
Stephen <stephen@umw.edu>
2016-05-02 16:00:42 -0400

    Fixed misinterpretation about connected graphs.

--- marchette/search.R
+++ marchette/search.R
@@ -10,16 +10,17 @@ set.seed(1234)
 # algebraic connectivity and the multiplicity of its algebraic connectivity.
 TOLERANCE <- 1e-10
 alg.conn <- function(laplacian) {
-    evals <- eigen(laplacian)$values
-    alg.conn.eval <- rev(unique(evals[which(abs(evals) > TOLERANCE)]))[2]
+    evals <- round(eigen(laplacian)$values, -log10(TOLERANCE))
+    alg.conn.eval <- rev(unique(evals))[2]
     return(list(eval=alg.conn.eval,
-        multiplicity=sum(abs(evals - alg.conn.eval) < TOLERANCE)))
+        multiplicity=sum(evals == alg.conn.eval)))
 }
 
-generate.random.graph <- function() erdos.renyi.game(20, .15)
+generate.random.graph <- function() erdos.renyi.game(20, .25)
 
 LIMIT <- 1e4
 alg.conns <- vector(length=LIMIT)
+vertex.conns <- vector(length=LIMIT)
 ctr <- 0
 g <- generate.random.graph()
 alg.conn.g <- alg.conn(laplacian_matrix(g))
@@ -35,9 +36,11 @@ while (alg.conn.g$multiplicity == 1  &&  ctr < LIMIT) {
 
     # Just for interest...
     alg.conns[ctr] <- alg.conn.g$eval
+    vertex.conns[ctr] <- vertex.connectivity(g)
 }
 
 if (alg.conn.g$multiplicity > 1) {
+    cat("FOUND ONE!\n")
     plot(g, 
         main=bquote(lambda[2]==.(round(alg.conn.g$eval,5)) ~ "of multiplicity"
         ~ .(round(alg.conn.g$multiplicity))))
@@ -46,3 +49,7 @@ if (alg.conn.g$multiplicity > 1) {
 }
 
 hist(alg.conns, breaks=20, main="algebraic connectivities")
+readline()
+plot(vertex.conns, alg.conns, xlab="vertex connectivity",
+    ylab="algebraic connectivity")
+abline(0,1)
